df0e1d1b0a6c7ca3d592c9857843b542d4f0e666
Samuel Nascimento <me@samnsc.com>
2025-01-10 11:16:17 -0300

    Fix expo not compilling

--- apps/user-frontend/app/_layout.tsx
+++ apps/user-frontend/app/_layout.tsx
@@ -10,13 +10,12 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { useColorScheme } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  let teste: App;
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
